0e4c6171d2a02f9f118728f5bcd528328bedb055
Mauricio Costa <mauriciof1910@hotmail.com>
2020-09-18 18:00:02 -0300

    resolução

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MilhasParaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- MilhasParaKm");
+            double doubleA = 1;
+            double doubleB = 1.609;
+            double doubleMultiplicacao = doubleA * doubleB;
+            Console.WriteLine($"{doubleA} * {doubleB} = {doubleMultiplicacao}");
+        }
+    }
+}
